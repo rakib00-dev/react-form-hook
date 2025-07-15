f081be938db813a5cc643c8e99c9279db09253ea
MD. Rejoanul Hasan Rakib <mdrakib80mondol@gmail.com>
2025-07-15 20:48:02 +0600

    updated the node 20.10.0 to 20.19.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
-const App = () => {
-  return <div>App</div>;
-};
+import { useForm } from 'react-hook-form';
+
+function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  return (
+    <form onSubmit={handleSubmit((data) => console.log(data))}>
+      <input {...register('firstName')} />
+      <input {...register('lastName', { required: true })} />
+      {errors.lastName && <p>Last name is required.</p>}
+      <input {...register('age', { pattern: /\d+/ })} />
+      {errors.age && <p>Please enter number for age.</p>}
+      <input type="submit" />
+    </form>
+  );
+}
 
 export default App;
